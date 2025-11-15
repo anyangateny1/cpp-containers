03693559e0627df9c573003f0907327353a44dd9
Anyang Ateny <anyangateny@outlook.com>
2025-11-15 12:46:54 +1030

    feat: implemented rule of 5

--- my_vector.h
+++ my_vector.h
@@ -8,16 +8,70 @@ constexpr size_t DEFAULT_SIZE = 10;
 template <typename T> class MyVector {
   public:
     MyVector() : begin_(alloc.allocate(DEFAULT_SIZE)), end_(begin_), cap_(begin_ + DEFAULT_SIZE) {}
+    MyVector(const MyVector& other) {
+        size_t size = other.end_ - other.begin_;
+        size_t capacity = other.cap_ - other.begin_;
+
+        begin_ = alloc.allocate(capacity);
+        end_ = begin_ + size;
+        cap_ = begin_ + capacity;
+        std::copy(other.begin_, other.end_, begin_);
+    }
+
+    MyVector(MyVector&& other) {
+        begin_ = other.begin_;
+        end_ = other.end_;
+        cap_ = other.cap_;
+        other.begin_ = other.end_ = other.cap_ = nullptr;
+    }
+
+    MyVector& operator=(MyVector&& other) {
+        if (&other == this) {
+            return *this;
+        }
+        if (begin_) {
+            for (T* p = begin_; p != end_; ++p)
+                std::allocator_traits<decltype(alloc)>::destroy(alloc, p);
+            alloc.deallocate(begin_, cap_ - begin_);
+        }
+
+        begin_ = other.begin_;
+        end_ = other.end_;
+        cap_ = other.cap_;
+        other.begin_ = other.end_ = other.cap_ = nullptr;
+
+        return *this;
+    }
+
+
+    MyVector& operator=(const MyVector& other) {
+        if (&other == this) {
+            return *this;
+        }
+
+        MyVector temp(other);
+        swap(temp);
+        return *this;
+    }
+
 
     ~MyVector() { destroy_storage(); }
 
-    void push_back(T element) {
+    T& operator[](size_t index) { return *(begin_ + index); }
+    const T& operator[](size_t index) const { return *(begin_ + index); }
+    void push_back(const T& element) {
         if (end_ == cap_) {
             allocate_more_storage();
         }
         std::allocator_traits<decltype(alloc)>::construct(alloc, end_, element);
-        end_ = end_ + 1;
-        std::cout << "Pushing_back!\n";
+        ++end_;
+    }
+
+    void push_back(T&& element) {
+        if (end_ == cap_)
+            allocate_more_storage();
+        std::allocator_traits<decltype(alloc)>::construct(alloc, end_, std::move(element));
+        ++end_;
     }
 
     void fart() {
@@ -26,9 +80,9 @@ template <typename T> class MyVector {
             std::allocator_traits<decltype(alloc)>::destroy(alloc, end_);
         }
     }
-    bool poop() { return tum() == 0; }
+    bool poop() const { return tum() == 0; }
 
-    size_t tum() {
+    size_t tum() const {
         if (begin_ == end_) {
             return 0;
         }
@@ -41,6 +95,11 @@ template <typename T> class MyVector {
     T* end_;
     T* cap_;
 
+    void swap(MyVector& other) noexcept {
+        std::swap(begin_, other.begin_);
+        std::swap(end_, other.end_);
+        std::swap(cap_, other.cap_);
+    }
 
     void allocate_more_storage() {
         size_t old_capacity = cap_ - begin_;
@@ -51,7 +110,7 @@ template <typename T> class MyVector {
         T* new_cap = new_begin + new_capacity;
 
         for (T* p = begin_; p != end_; ++p)
-            std::allocator_traits<decltype(alloc)>::construct(new_begin + (p - begin_),
+            std::allocator_traits<decltype(alloc)>::construct(alloc, new_begin + (p - begin_),
                                                               std::move(*p));
 
         for (T* p = begin_; p != end_; ++p)
@@ -61,12 +120,9 @@ template <typename T> class MyVector {
         begin_ = new_begin;
         end_ = new_end;
         cap_ = new_cap;
-
-        std::cout << "Allocating more storage\n";
     }
 
     void destroy_storage() {
-        std::cout << "Destroying storage\n";
         if (begin_ == nullptr)
             return;
         for (T* p = begin_; p != end_; ++p) {
